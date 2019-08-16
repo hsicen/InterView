a78a262dbfc39379f65dfa43867bf771a4fe506e
Night <codinghuang@163.com>
2019-08-16 14:00:32 +0800

    Initial commit

---
+++ app/src/main/java/com/hsicen/interview/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.hsicen.interview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * <p>作者：Hsicen  2019/8/16 13:55
+ * <p>邮箱：codinghuang@163.com
+ * <p>功能：
+ * <p>描述：每日一问
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
