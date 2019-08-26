b381084ebb26f40181323c6afd44cd05001d9b51
Hsicen <codinghuang@163.com>
2019-08-26 23:02:08 +0800

    单链表数据结构的实现

---
+++ app/src/main/java/com/hsicen/interview/linklist/LruLinkedList.kt
@@ -0,0 +1,16 @@
+package com.hsicen.interview.linklist
+
+/**
+ * <p>作者：Hsicen  2019/8/26 22:35
+ * <p>邮箱：codinghuang@163.com
+ * <p>作用：
+ * <p>描述：链表
+ *
+ *   使用单链表实现LRU算法(默认大小为5)
+ */
+
+class LruLinkedList(val capacity: Int = 5) {
+
+
+
+}
