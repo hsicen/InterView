b271e7a40a55f6a0e91006d69bd366fa0100550e
Hsicen <codinghuang@163.com>
2019-09-03 10:10:37 +0800

    内存分配和Java中的参数传递分析

--- app/src/main/java/com/hsicen/interview/array/Test.java
+++ app/src/main/java/com/hsicen/interview/array/Test.java
@@ -1,5 +1,7 @@
 package com.hsicen.interview.array;
 
+import java.util.Arrays;
+
 /**
  * <p>作者：Hsicen  2019/8/25 22:42
  * <p>邮箱：codinghuang@163.com
@@ -14,5 +16,17 @@ public class Test {
 
         System.out.println("1==   " + str1 == str2);
         System.out.println("equals   " + str1.equals(str2));
+
+        int[] data = {1, 2, 3, 4, 5, 6, 7};
+
+        System.out.println("main: " + Arrays.hashCode(data));
+        equalArray(data);
+        System.out.println("main: " + Arrays.hashCode(data));
+    }
+
+    private static void equalArray(int[] src) {
+        System.out.println("method: " + Arrays.hashCode(src));
+        src = new int[10];
+        System.out.println("method: " + Arrays.hashCode(src));
     }
 }
---
+++ app/src/main/java/com/hsicen/interview/sort/InsertSort.java
@@ -0,0 +1,52 @@
+package com.hsicen.interview.sort;
+
+import java.util.Arrays;
+
+/**
+ * <p>作者：Hsicen  2019/9/2 15:32
+ * <p>邮箱：codinghuang@163.com
+ * <p>作用：
+ * <p>描述：插入排序算法分析
+ * 基于比较的排序算法
+ * 将数组分成两个部分，有序部分和无序部分，
+ * 每次取出无序数组的第一个元素，然后从后往前和有序数据进行比较找到新数据的插入位置。
+ * 初始有序部分可以为数组的第一个元素
+ * <p>
+ * 时间复杂度：最好O(n)   最坏O(n^2)  平均O(n^2)
+ * 空间复杂度：O(1) 原地排序
+ * 比较交换次数：逆序度
+ * 稳定性：稳定排序算法
+ */
+public class InsertSort {
+
+    public static void main(String[] args) {
+        int[] data = {1, 3, 5, 2, 4, 6};
+        System.out.println("data = " + Arrays.toString(data));
+        sort(data, 6);
+        System.out.println("sort = " + Arrays.toString(data));
+    }
+
+    private static int[] sort(int[] data, int size) {
+        if (1 == size) return data;
+
+        int i;  //无序数组开始下标
+        int j; //有序数组最后一个元素下标
+
+        //遍历无序数组
+        for (i = 1; i < size; i++) {
+            int value = data[i];
+            j = i - 1;
+
+            //遍历有序数组(从后往前)，寻找插入位置
+            for (; j >= 0; j--) {
+                if (data[j] > value) {
+                    data[j + 1] = data[j];  //移动老数据
+                } else break;
+            }
+
+            data[j + 1] = value; //插入新数据
+        }
+
+        return data;
+    }
+}
