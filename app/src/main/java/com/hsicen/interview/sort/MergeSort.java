cb2ca72aa8f953d38def030314d6181267054653
Hsicen <codinghuang@163.com>
2019-09-10 17:21:45 +0800

    利用哨兵简化归并排序的有序数组合并

--- app/src/main/java/com/hsicen/interview/sort/MergeSort.java
+++ app/src/main/java/com/hsicen/interview/sort/MergeSort.java
@@ -38,11 +38,11 @@ public class MergeSort {
         merge(src, middle + 1, end);
 
         //合并分组
-        sort(src, start, middle, end);
+        sortBySentry(src, start, middle, end);
     }
 
     /**
-     * 合并数组
+     * 合并两个有序数组
      *
      * @param dest   原数组
      * @param start  其实下标
@@ -55,6 +55,7 @@ public class MergeSort {
         int k = 0;
         int[] tmp = new int[end - start + 1];
 
+        //合并有序数组
         while (i <= middle && j <= end) {
             if (dest[i] <= dest[j]) {
                 tmp[k++] = dest[i++];
@@ -63,25 +64,22 @@ public class MergeSort {
             }
         }
 
-        int destStart = i;
-        int destEnd = middle;
-
-        if (j <= end) {
-            destStart = j;
-            destEnd = end;
-        }
+        //判断未合并完的数组
+        int destStart = (j <= end) ? j : i;
+        int destEnd = (j <= end) ? end : middle;
 
         while (destStart <= destEnd) {
             tmp[k++] = dest[destStart++];
         }
 
+        //将排序好的数组拷贝到原数组中
         for (i = 0; i <= end - start; i++) {
             dest[start + i] = tmp[i];
         }
     }
 
     /**
-     * 利用哨兵合并两个数组
+     * 利用哨兵合并两个有序数组
      *
      * @param dest   原数组
      * @param start  开始下标
@@ -89,7 +87,30 @@ public class MergeSort {
      * @param end    结束下标
      */
     private static void sortBySentry(int[] dest, int start, int middle, int end) {
+        int[] leftSrc = new int[middle - start + 2];
+        int[] rightSrc = new int[end - middle + 1];
 
+        //左边数组添加哨兵
+        leftSrc[middle - start + 1] = Integer.MAX_VALUE;
+        if (middle - start + 1 >= 0)
+            System.arraycopy(dest, start, leftSrc, 0, middle - start + 1);
 
+        //右边数组添加哨兵
+        rightSrc[end - middle] = Integer.MAX_VALUE;
+        for (int i = 0; i < end - middle; i++) {
+            rightSrc[i] = dest[middle + 1 + i];
+        }
+
+        //数组合并
+        int leftStart = 0;
+        int rightStart = 0;
+        int index = start;
+        while (index <= end) {
+            if (leftSrc[leftStart] <= rightSrc[rightStart]) {
+                dest[index++] = leftSrc[leftStart++];
+            } else {
+                dest[index++] = rightSrc[rightStart++];
+            }
+        }
     }
 }
